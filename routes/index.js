9721d8388da49386f291d50e24f03a95a24f2ccc
Miguel Osorio <ing.mosorio19@gmail.com>
2019-02-10 20:56:33 -0600

    [ADD] signup step 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const estados = require('../models/estados')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('Login/signup');
+  res.render('Login/signup_step_2', {estados});
 });
 
 module.exports = router;
